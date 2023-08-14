283f57cb2fa013d1cb82cfd5b68b71f1027b8b38
abitstupid <hrhemant0000at@gmail.com>
2023-08-14 10:45:10 +0530

    images added and resized

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ thumbBar.addEventListener("click", (e) =>{
 })
 
 // thumbBar 
-const images = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg","image6.jpg","image7.jpg","image8.jpg","image9.jpg","image10.jpg",];
+const images = ["image1.jpg","image2.jpg","image3.jpg","image4.jpg","image5.jpg","image6.jpg","image7.svg","image8.svg","image9.jpg","image10.jpg", "image11.svg"];
 
 for (const image of images) {
     const thumbImgContainer = document.createElement("div");
